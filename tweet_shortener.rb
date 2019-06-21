d8ba0024441ce0ff30f66646a56d76a77a4ded4e
Carlos Velazquez <skamateria@gmail.com>
2019-06-21 02:49:09 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,6 +1,3 @@
-phrase = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
-
-# Write your code here.
 
 
 def dictionary
