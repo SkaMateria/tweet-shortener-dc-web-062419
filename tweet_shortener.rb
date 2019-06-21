309a0f6c6a773d9fcde0397042dabb93b312f0c9
Carlos Velazquez <skamateria@gmail.com>
2019-06-21 02:47:27 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,13 +1,19 @@
+phrase = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
+
+# Write your code here.
+require "pry"
+
 def dictionary
   convert = {
-    "hello" ==> "hi",
-    "to" ==> "2",
-    "two" ==> "2",
-    "too" ==> "2",
-    "be" ==> "b",
-    "you" ==> "u",
-    "at" ==> "@",
-    "and" ==> "&"
-  }
-  
+   "hello" => "hi",
+   "to" => "2",
+   "two" => "2",
+   "too" => "2",
+   "for" => "4",
+   "four" => "4",
+   "be" => "b",
+   "you" => "u",
+   "at" => "@",
+   "and" => "&"
+}
 end
