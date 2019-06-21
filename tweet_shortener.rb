647ecee838b1cdf830b65525eb52b9e0bc4cbbc6
Carlos Velazquez <skamateria@gmail.com>
2019-06-21 02:48:18 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,7 @@
 phrase = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
 
 # Write your code here.
-require "pry"
+
 
 def dictionary
   convert = {
